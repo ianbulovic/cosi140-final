6b129f4a1c6f08b28ffdfdd0aa992e61bdf37eb3
Ian Bulovic <ianbulovic@brandeis.edu>
2023-05-04 17:22:11 -0400

    add conflict visualizer to adjudicator

--- adjudication/adjudicator.py
+++ adjudication/adjudicator.py
@@ -8,7 +8,7 @@ def read_annotation_file(filepath):
     with open(filepath, "r") as data_file:
         for tokenlist in conllu.parse_incr(data_file):
             # assemble sentence from tokens
-            sentence = ' '.join(str(token) for token in tokenlist)
+            sentence = tuple([str(token) for token in tokenlist])
             sentences.append(sentence)
             # assemble the set of dependencies
             dependencies = set([(x['head'], x['deprel'], x['id']) for x in tokenlist if not x['deprel'] == '_'])
@@ -19,6 +19,19 @@ def read_annotation_file(filepath):
 def has_conflict(a_deps, b_deps):
     '''Calculate the sentence-level agreement between two sets of dependencies.'''
     return not a_deps == b_deps
+
+def print_annotated_sentence(sentence, deps):
+    for first, deprel, second in deps:
+        if second < first:
+            first, second = second, first
+        print(" ".join([" " * len(word) for word in sentence[:first-1]]), end="")
+        print(" ", end="")
+        print("-".join(["-" * len(word) for word in sentence[first-1:second]]), end="")
+        print(f" {deprel}")
+        
+    print(" ".join(sentence))
+    print()
+
 
 def find_conflicts(file_a, file_b):
     a_sen, a_ann = read_annotation_file(file_a)
@@ -36,7 +49,9 @@ def find_conflicts(file_a, file_b):
 
     for i, pair in enumerate(zip(a_ann, b_ann)):
         if has_conflict(*pair):
-            print(f"{i}: {a_sen[i]}")
+            print_annotated_sentence(a_sen[i], a_ann[i])
+            print_annotated_sentence(b_sen[i], b_ann[i])
+            input()
 
 if __name__ == "__main__":
     file_a = "annotations/external/ds1-huiyu.txt.conllu"
